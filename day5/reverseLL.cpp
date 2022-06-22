6efd0994b0f8c562ffa01f55ad406efbfa68937d
Neha-Kaushik3 <61658106+Neha-Kaushik3@users.noreply.github.com>
2022-06-23 00:12:54 +0530

    Update reverseLL.cpp

--- day5/reverseLL.cpp
+++ day5/reverseLL.cpp
@@ -36,3 +36,35 @@ LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head)
     head = prev;
     return head;
 }
+#include <bits/stdc++.h>
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+LinkedListNode<int> *helper(LinkedListNode<int> *head)
+{
+    if (!head || !head->next)
+        return head;
+    LinkedListNode<int> *nHead = helper(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return nHead;
+}
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head)
+{
+    return helper(head);
+}
